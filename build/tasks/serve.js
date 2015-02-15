7ef01dba3a39949f76e8a65c94b4ccf07ff0553e
K. Scott Allen <scott@OdeToCode.com>
2015-02-15 11:15:19 -0500

    chore(build): fix file watch, avoid node restart

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -2,5 +2,8 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
 gulp.task('serve', ['build'], function() {
-    return nodemon({ script: 'server/main.js' });
+    return nodemon({
+        script: 'server/main.js',
+        ignore: ['./app', './benchmarks', './dist', './test']
+    });
 });
