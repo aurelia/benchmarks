2b5fde17ddc994e4ebcce0bdebe281ece950bdfb
K. Scott Allen <scott@OdeToCode.com>
2015-03-19 23:03:40 +0100

    ensure serial execution order

--- app/ui/testList.js
+++ app/ui/testList.js
@@ -22,7 +22,24 @@ export class TestList {
     }
 
     runTests() {
-        Promise.all(this.microTests.map(t => t.run()));
-        Promise.all(this.macroTests.map(t => t.run()));
+        this.run(this.microTests);
+        this.run(this.macroTests);
+    }
+
+    run(tests) {
+        let currentIndex = 0;
+
+        let next = () => {
+            currentIndex += 1;
+            execute();
+        };
+
+        let execute = () => {
+            if(currentIndex < tests.length) {
+                tests[currentIndex].run().then(next);
+            }
+        };
+        
+        execute();
     }
 }
