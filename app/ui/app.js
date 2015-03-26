ed0330cb25c9c40d4105872da761f1d4faa31e77
K. Scott Allen <scott@OdeToCode.com>
2015-03-26 09:05:01 -0400

    feat(ui): adding route for result comparisons

--- app/ui/app.js
+++ app/ui/app.js
@@ -2,6 +2,7 @@ import {Router} from 'aurelia-router';
 
 let routes = [
         { route: [''],  moduleId: './testList', nav: true, title:'Test List' },
+        { route: ['/compare'], moduleId: './compare', nav: true, title: 'Compare Results'}
     ];
 
 export class App {
