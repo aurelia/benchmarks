f4af9414bcd664e1950c63c420c996543af1c6f7
Jeremy Danyow <jdanyow@gmail.com>
2015-07-29 23:13:13 -0400

    feat(macro): simple table benchmark

--- benchmarks/macro/simple-form/app.js
+++ benchmarks/macro/simple-form/app.js
@@ -1,8 +1,8 @@
 export class App {
   fromAirport = 'JFK';
   toAirport = 'LHR';
-  departureDate = new Date(2015, 7, 29);
-  returnDate = new Date(2015, 8, 13);
+  departureDate = '7/29/2015';
+  returnDate = '8/13/2015';
   passengers = 2;
   includeNearby = false;
   flexibleDates = true;
---
+++ benchmarks/macro/simple-table/app.js
@@ -0,0 +1,12 @@
+export class App {
+  planets = [
+    { name: 'Mercury', diameter: 3032, distance: 35983610 },
+    { name: 'Venus', diameter: 7521, distance: 67232360 },
+    { name: 'Earth', diameter: 7926, distance: 92957100 },
+    { name: 'Mars', diameter: 4222, distance: 141635300 },
+    { name: 'Jupiter', diameter: 88846, distance: 483632000 },
+    { name: 'Saturn', diameter: 74898, distance: 888188000 },
+    { name: 'Uranus', diameter: 31763, distance: 1783950000 },
+    { name: 'Neptune', diameter: 30778, distance: 2798842000 },
+  ];
+}
