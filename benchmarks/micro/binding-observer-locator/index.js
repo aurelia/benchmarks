b078b9d1720b87ba20392032cf15ca0049c6d7db
Jeremy Danyow <jdanyow@gmail.com>
2015-09-12 15:33:27 -0400

    binding api changes

--- benchmarks/micro/binding-observer-locator/index.js
+++ benchmarks/micro/binding-observer-locator/index.js
@@ -42,7 +42,7 @@ class Test {
 
 var locator = new ObserverLocator(new TaskQueue(), new EventManager(), new DirtyChecker(), [new MockAdapter()]);
 
-export default deferred => {
+let oldBench = deferred => {
   var i = 100, object, observer, cb = (newValue, oldValue) => {};
   while(i--) {
     object = new Test();
@@ -74,3 +74,50 @@ export default deferred => {
   }
   deferred.resolve();
 };
+
+let newBench = deferred => {
+  var i = 100, object, observer, cb = (newValue, oldValue) => {};
+  while(i--) {
+    object = new Test();
+    observer = locator.getObserver(object, 'simple');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'getterSetter');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'computed');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'adapter');
+    observer = locator.getObserver(object, 'undefinedProperty');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+
+    object = document.createElement('input');
+    object.simple = 'hello world';
+    Object.defineProperty(object, 'getterSetter', { get: () => null, set: newValue => {} });
+    observer = locator.getObserver(object, 'value');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'checked');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'class');
+    observer = locator.getObserver(object, 'css');
+    observer = locator.getObserver(object, 'simple');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'getterSetter');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+    observer = locator.getObserver(object, 'undefinedProperty');
+    observer.subscribe(cb);
+    observer.unsubscribe(cb);
+  }
+  deferred.resolve();
+};
+
+
+let hasUnsubscribe = !!locator.getObserver({ foo: 'bar' }).unsubscribe;
+let bench = hasUnsubscribe ? newBench : oldBench;
+export default bench;
